a202907e8caa25ddb55c2cd86583be86e2a1d470
Wonbin Lee <lob3767@gmail.com>
2022-03-26 19:24:13 +0900

    [Add] SignInView

--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
 import json, bcrypt, jwt
 from django.forms import ValidationError
 
-
 from django.http import JsonResponse
 from django.views import View
 from django.db import IntegrityError
+from django.conf import settings
 
 from .models import User
 from utils.validators import email_validator, password_validator, phone_number_validator
@@ -38,3 +38,25 @@ class SignUpView(View):
         except ValidationError as e :
             return JsonResponse({"message" : e.message}, status = 400)
 
+class SignInView(View):
+    def post(self, request):
+        try:
+            data     = json.loads(request.body)
+            email    = data["email"]
+            password = data["password"]
+            
+            email_validator(email)
+            password_validator(password)
+
+            user = User.objects.get(email = email)
+
+            if not bcrypt.checkpw(password.encode("utf-8"), user.password.encode("utf-8")):
+                return JsonResponse({"message": "IVALID_USER"}, status = 401)
+            
+            access_token = jwt.encode({"id" : user.id}, settings.SECRET_KEY, settings.ALGORITHM)
+            return JsonResponse({"token" : access_token}, status = 200)
+
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
+        except User.DoesNotExist:
+            JsonResponse({"message": "IVALID_USER"}, status = 401)
--- utils/models.py
+++ utils/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from django.db import models
-
 class TimestampZone(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
